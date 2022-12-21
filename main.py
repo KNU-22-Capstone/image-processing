da033dfee4207f00cbd07291fb68eb283c14ba07
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2022-12-21 16:01:56 +0900

    테스트 환경

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import remove_background
 import ton_tag
 
 # url = 'https://image.msscdn.net/images/goods_img/20221123/2958053/2958053_1_500.jpg?t=20221206133853' #무채색 신발
-url = 'https://image.msscdn.net/images/goods_img/20221201/2968195/2968195_1_500.jpg?t=20221201154238'
+url = 'https://image.msscdn.net/images/goods_img/20180917/859956/859956_9_500.jpg'
 image = image_open.img_url_open(url, True)
 
 image = remove_background.image_remove(image, True)
